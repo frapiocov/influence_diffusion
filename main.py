7fb4e76548fc3c145903f956ecf709d0c3eea0d4
Francesco Pio Covino <67202985+frapiocov@users.noreply.github.com>
2024-09-07 15:31:06 +0200

    data explore

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import snap
+import copy
+import math
+import time
+import random
 
-print("----- vector ----- ")
+
+# total cost of Seed set
+def seed_set_cost(ss, cost):
+    totalCost = 0
+    for i in ss:
+        totalCost += cost[i]
+
+    return totalCost
 
 
+# genera un costo casuale da 1 a 30
+def rand_cost_func():
+    p = random.randint(1, 30)
+    return p
 
-v = snap.TIntV()
 
-v.append(1)
-v.append(2)
-v.append(3)
-v.append(4)
-v.append(5)
-
-print(len(v))
-print(v[2])
+# costo  è la metà del grado del nodo
+def half_deg_cost_func(deg):
+    if deg != 0:
+        return deg / 2
+    else:
+        return 1
